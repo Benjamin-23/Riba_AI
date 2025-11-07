3e6b56d194704fd534ee17c0154639c6998eae9a
benjamin-23 <kitongabenja34@gmail.com>
2025-11-07 11:58:01 +0300

    signin and signup

---
+++ riba-front/app/signin/page.tsx
@@ -0,0 +1,8 @@
+export default function Signin(){
+    return (
+        <div className="  flex flex-col items-center justify-center min-h-screen py-2 text-background">
+          <h1>Sign Up Page</h1>
+          <p>Create your account to get started</p>
+        </div>
+      );
+}
---
+++ riba-front/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function Signup(){
+    return (
+        <div className="  flex flex-col items-center justify-center min-h-screen py-2 text-background">
+          <h1>Sign Up Page</h1>
+          <p>Create your account to get started</p>
+          
+        </div>
+      );
+}
